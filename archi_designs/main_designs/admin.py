161991c73a56773712625ca1b1c0fd4fccae142e
eglemot <egle.mot97@gmail.com>
2023-03-14 15:16:04 +0300

    added models, media, created admin interface

--- archi_designs/main_designs/admin.py
+++ archi_designs/main_designs/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Plan, FloorPicture, PlanPicture
 
-# Register your models here.
+class FloorPictureInline(admin.TabularInline):
+    model = FloorPicture
+
+class PlanPictureInline(admin.TabularInline):
+    model = PlanPicture
+
+class FloorPictureAdmin(admin.ModelAdmin):
+    list_display = ('plan', 'floor_number')
+    list_filter = ('plan',)
+    search_fields = ('plan__name',)
+    
+class PlanPictureAdmin(admin.ModelAdmin):
+    list_display = ('plan',)
+    list_filter = ('plan',)
+    search_fields = ('plan__name',)
+
+class PlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'heated_sq_feet', 'floors', 'min_bedrooms', 'max_bedrooms', 'min_bathrooms', 'max_bathrooms', 'basement', 'loft', 'walk_in_pantry', 'home_office', 'mudroom', 'bonus_room', 'wrap_around_porch', 'style')
+    search_fields = ('name',)
+    inlines = [
+        PlanPictureInline,
+        FloorPictureInline,
+    ]
+    fieldsets = (
+        ('Add plan', {
+            'fields': ('name', 'heated_sq_feet', 'affiliate_link')
+        }),
+        ('Details', {
+            'fields': ('floors', 'min_bedrooms', 'max_bedrooms', 'min_bathrooms', 'max_bathrooms', 'description')
+        }),
+        ('Options', {
+            'fields': ('basement', 'loft', 'walk_in_pantry', 'home_office', 'mudroom', 'bonus_room', 'wrap_around_porch', 'style')
+        }),
+    )
+
+admin.site.register(Plan, PlanAdmin)
+admin.site.register(FloorPicture, FloorPictureAdmin)
+admin.site.register(PlanPicture, PlanPictureAdmin)
+
+
--- archi_designs/main_designs/models.py
+++ archi_designs/main_designs/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Plan(models.Model):
+
+    FLOOR_CHOICES = (
+        ('1', '1'),
+        ('2', '2'),
+        ('3', '3'),
+    )
+
+    STYLE_CHOICES = (
+        ('barndominium', 'Barndominium'),
+    )
+
+    name = models.CharField(max_length=255)
+    min_bedrooms = models.PositiveSmallIntegerField()
+    max_bedrooms = models.PositiveSmallIntegerField()
+    floors = models.CharField(max_length=10, choices=FLOOR_CHOICES)
+    min_bathrooms = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
+    max_bathrooms = models.DecimalField(max_digits=3, decimal_places=1, null=True, blank=True)
+    heated_sq_feet = models.DecimalField(max_digits=20, decimal_places=3, null=True, blank=True)
+    basement = models.BooleanField(default=False)
+    loft = models.BooleanField(default=False)
+    walk_in_pantry = models.BooleanField(default=False)
+    home_office = models.BooleanField(default=False)
+    mudroom = models.BooleanField(default=False)
+    bonus_room = models.BooleanField(default=False)
+    wrap_around_porch = models.BooleanField(default=False)
+    affiliate_link = models.URLField()
+    style = models.CharField(max_length=20, choices=STYLE_CHOICES, default='barndominium')
+    description = models.TextField(blank=True, default='')
+
+class FloorPicture(models.Model):
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE, related_name='floor_pictures')
+    floor_number = models.PositiveIntegerField()
+    image = models.ImageField(upload_to='pictures/floor_pictures')
+
+class PlanPicture(models.Model):
+    image = models.ImageField(upload_to='pictures/plan_pictures')
+    plan = models.ForeignKey(Plan, on_delete=models.CASCADE, related_name='plan_pictures')
