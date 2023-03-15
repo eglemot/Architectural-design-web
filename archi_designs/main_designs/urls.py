1a33bcf09eaaf4a2c5b37a41a1aab31b20097311
eglemot <egle.mot97@gmail.com>
2023-03-15 12:58:57 +0300

    created plan_list view, html, fixed home html

--- archi_designs/main_designs/urls.py
+++ archi_designs/main_designs/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.index, name='home'),
+    path('home', views.home, name='home'),
+    path('plan_list', views.plan_list, name='plan_list'),
 ]
--- archi_designs/main_designs/views.py
+++ archi_designs/main_designs/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .static import models
+from . import models
+from django.db.models import Q
 
 
-def index(request):
+def home(request):
     plan_count = models.Plan.objects.count()
-    return render(request, 'main_designs/index.html', {
+    return render(request, 'main_designs/home.html', {
         'plan_count': plan_count,
     })
+
+def plan_list(request):
+    plans = models.Plan.objects.all().prefetch_related('floor_pictures', 'plan_pictures')
+    query = request.GET.get('q')
+    if query:
+        plans = plans.filter(Q(name__icontains=query) |
+                             Q(description__icontains=query) |
+                             Q(style__icontains=query))
+    return render(request, 'main_designs/plan_list.html', {'plans': plans})
+
+
+
