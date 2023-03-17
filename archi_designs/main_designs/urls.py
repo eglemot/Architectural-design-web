d80129f039de0c8ec1973aeff164307b37a5ff09
eglemot <egle.mot97@gmail.com>
2023-03-17 11:37:22 +0300

    created plan_list.html (last one)

--- archi_designs/main_designs/urls.py
+++ archi_designs/main_designs/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name='home'),
-    path('plan_list', views.plan_list, name='plan_list'),
+    path('home/', views.home, name='home'),
+    path('plan_list/', views.plan_list, name='plan_list'),
 ]
