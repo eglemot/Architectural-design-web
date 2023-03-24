61db6629d612afaceaaa1ca36000accadeb5cd26
eglemot <egle.mot97@gmail.com>
2023-03-24 12:52:13 +0300

    trying with elastic branstalk

--- archi_designs/archi_designs/settings.py
+++ archi_designs/archi_designs/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = local_settings.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-env.eba-xuc2i26c.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
