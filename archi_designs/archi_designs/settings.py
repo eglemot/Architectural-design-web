95dda7884285f84b90fed7f8836c4a394eeb8c4a
eglemot <egle.mot97@gmail.com>
2023-03-24 21:44:20 +0300

    heroku

--- archi_designs/archi_designs/settings.py
+++ archi_designs/archi_designs/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
-from . import local_settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = local_settings.SECRET_KEY
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -133,14 +132,15 @@ EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_USE_TLS=True
-EMAIL_HOST_USER=local_settings.EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD=local_settings.EMAIL_HOST_PASSWORD
-DEFAULT_FROM_EMAIL=local_settings.DEFAULT_FROM_EMAIL
+EMAIL_HOST_USER=os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD=os.getenv('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL=os.getenv('DEFAULT_FROM_EMAIL')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_FROM_EMAIL = 'egle.mot97@gmail.com'
 
 # Cookies
 SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
