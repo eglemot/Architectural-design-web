35aa9a77d95efa7a560b86d13d9b6a5f07d23773
eglemot <egle.mot97@gmail.com>
2023-03-25 11:03:05 +0300

    heroku

--- archi_designs/archi_designs/settings.py
+++ archi_designs/archi_designs/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 import os
 from pathlib import Path
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,23 +80,25 @@ WSGI_APPLICATION = 'archi_designs.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# } 
-
 DATABASES = {
     'default': {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "HOST": "ec2-34-251-233-253.eu-west-1.compute.amazonaws.com",
-        "NAME": "dc2qea55sph5cl",
-        "USER": "mxjkeuhcblbhbt",
-        "PASSWORD": "059fa98ee0e302c78cc8b13987f261c47cdd44617366ca92eb03f74c299f6690",
-        "PORT": 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+} 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+
+# DATABASES = {
+#     'default': {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "HOST": "ec2-34-251-233-253.eu-west-1.compute.amazonaws.com",
+#         "NAME": "dc2qea55sph5cl",
+#         "USER": "mxjkeuhcblbhbt",
+#         "PASSWORD": "059fa98ee0e302c78cc8b13987f261c47cdd44617366ca92eb03f74c299f6690",
+#         "PORT": 5432,
+#     }
+# }
 
 
 # Password validation
