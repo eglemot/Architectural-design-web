4c94b588961e0fefb9921d9ff5e685c2979ec3cf
eglemot <egle.mot97@gmail.com>
2023-03-26 22:25:17 +0300

    fixing robots.txt

--- archi_designs/main_designs/urls.py
+++ archi_designs/main_designs/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
@@ -8,4 +9,5 @@ urlpatterns = [
     path('contacts/', views.ContactView.as_view(), name='contact'),
     path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
     path('affiliate-disclosure/', views.affiliate_disclosure, name='affiliate_disclosure'),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots"),
 ]
