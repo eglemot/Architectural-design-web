8f01a60b114d8317d87bbe86e172f717c7a7fc33
eglemot <egle.mot97@gmail.com>
2023-03-26 22:17:12 +0300

    added robots.txt to urls

--- archi_designs/archi_designs/urls.py
+++ archi_designs/archi_designs/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', include('main_designs.urls')),
     path('admin/', admin.site.urls),
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type="text/plain"), name="robots"),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
